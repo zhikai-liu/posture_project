bf6e2e879bd6e3e87642272d1f6f96ddabfd5c2c
zhikai <16259287+zhikai-liu@users.noreply.github.com>
2025-02-17 22:38:23 -0500

    upload example data

--- nerve_analysis/main_analysis_example.m
+++ nerve_analysis/main_analysis_example.m
@@ -1,7 +1,7 @@
 %Example code for analysis
-M_file='ZL240626_M1_example.mat';
+M_file='example_data.mat';
 addpath('./utility/')
 process_raw_data_multi_unit(M_file);
-process_multi_unit(['analyzed_' M_file],[],[],[],[]);
+process_multi_unit(['analyzed_' M_file]);
 process_normalize_cardiac_cycle(['multi_unit_analyzed_' M_file])
 plot_normalized_hist(['multi_unit_analyzed_' M_file])
